097efaa6077d1787c1cfb2f9c2c9f1ff16e8d6ac
Joshua Smith <69738912+knd94@users.noreply.github.com>
2025-02-06 02:33:25 +0000

    Created GameObjects, a bool...
    And started working on Helper Methods.

--- Assets/Scripts/MazeCell.cs
+++ Assets/Scripts/MazeCell.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class MazeCell : MonoBehaviour
 {
-    
+    [SerializeField]
+    private GameObject _leftWalll;
+
+    [SerializeField]
+    private GameObject _rightWall;
+
+    [SerializeField]
+    private GameObject _frontWall;
+
+    [SerializeField]
+    private GameObject _backWall;
+
+    [SerializeField]
+    private GameObject _unvisitedMazeCell;
+
+    // Cheeck to see if Cell has been visited by generation via a boolean.
+    public bool IsVisited { get; private set; }
+
+    // Helper Methods
+    public void Visit()
+    {
+
+    }
 }
